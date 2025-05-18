43d8351a456633047532f8e66d5d6119653bb3c4
Oleg Kotok <kotok_oleg@ukr.net>
2025-05-18 21:15:14 +0300

    Add contact form

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+const form = document.querySelector("form");
+if (form) {
+  form.addEventListener("submit", (e) => {
+    const submitBtn = form.querySelector("button[type=submit]");
+    if (submitBtn) {
+      submitBtn.disabled = true;
+      submitBtn.innerText = "Надсилається...";
+    }
+  });
+}
